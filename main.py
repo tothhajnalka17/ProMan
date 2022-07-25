3f31ad136f1ea49c53eca6b92945225efeec7ceb
GergelyKamaras <gergely.kamaras@gmail.com>
2022-07-25 18:07:49 +0200

    Add: server side routing

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 from dotenv import load_dotenv
 from util import json_response
 import mimetypes
@@ -23,6 +23,12 @@ def get_boards():
     All the boards
     """
     return queries.get_boards()
+
+
+@app.route('/create_board', methods=["POST"])
+def create_board():
+    board_title = request.form.get("boardTitle")
+    return queries.insert_board(board_title)
 
 
 @app.route("/api/boards/<int:board_id>/cards/")
