f91b63d555cfed56f7f76b0ef54612dbd309b76b
Dfulop98 <dobo.fulop@icloud.com>
2022-07-25 05:59:23 -0700

    feat:login, signup route

--- main.py
+++ main.py
@@ -15,6 +15,13 @@ def index():
     """
     return render_template('index.html')
 
+@app.route("/login")
+def login():
+    return render_template('login.html')
+
+@app.route("/signup")
+def signup():
+    return render_template('signup.html')
 
 @app.route("/api/boards")
 @json_response
