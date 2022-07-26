3eccb16fef2ec9bd7c571b74e7ed334ea4216af8
Dfulop98 <dobo.fulop@icloud.com>
2022-07-26 05:52:01 -0700

    feat: implement card to right position

--- static/js/controller/cardsManager.js
+++ static/js/controller/cardsManager.js
@@ -6,9 +6,10 @@ export let cardsManager = {
     loadCards: async function (boardId) {
         const cards = await dataHandler.getCardsByBoardId(boardId);
         for (let card of cards) {
+            let index = card.status_id
             const cardBuilder = htmlFactory(htmlTemplates.card);
             const content = cardBuilder(card);
-            domManager.addChild(`.board[data-board-id="${boardId}"]`, content);
+            domManager.addChild(`.board[data-board-id="${boardId}"] > .board-column:nth-of-type(${index})`, content);
             domManager.addEventListener(
                 `.card[data-card-id="${card.id}"]`,
                 "click",
