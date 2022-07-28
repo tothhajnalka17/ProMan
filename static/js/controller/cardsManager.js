108d07e431c1c01ed89b09c3c9081b0925e8637f
GergelyKamaras <gergely.kamaras@gmail.com>
2022-07-28 09:53:54 +0200

    Add: reminder

--- static/js/controller/cardsManager.js
+++ static/js/controller/cardsManager.js
@@ -21,6 +21,7 @@ export let cardsManager = {
 
     },
     insertAddCardButton: function (boardId, status) {
+        // TODO select column based on its id value, not the nth-of type
         let firstColumn = document.querySelector(`.board[data-board-id="${boardId}"] > .board-column:nth-of-type(1)`);
         let cardOrder = document.querySelectorAll(`.board[data-board-id="${boardId}"] > .board-column:nth-of-type(1) > .card`).length + 1;
         let button = document.createElement("button");
