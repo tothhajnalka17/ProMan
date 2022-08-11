ab9bb8e27159e74046adbf0cad21bf280ddcfd0a
GergelyKamaras <gergely.kamaras@gmail.com>
2022-08-11 10:02:34 +0200

    Fix: card renaming bug after card insert

--- static/js/controller/cardsManager.js
+++ static/js/controller/cardsManager.js
@@ -3,6 +3,28 @@ import {htmlFactory, htmlTemplates} from "../view/htmlFactory.js";
 import {domManager} from "../view/domManager.js";
 import {initDraggable} from "./dragNDropManager.js";
 
+function initCardRename(cardTitleDiv) {
+    cardTitleDiv.addEventListener("click", () => {
+        cardTitleDiv.addEventListener("keydown", async (event) => {
+            if (event.key === "Enter") {
+                event.preventDefault();
+                let cardId = event.target.parentElement.dataset.cardId;
+                let boardId = event.target.parentElement.dataset.boardId;
+                let statusId = event.target.parentElement.dataset.statusId;
+                let cardOrder = event.target.parentElement.dataset.cardOrder;
+                let newTitle = cardTitleDiv.innerText;
+                try {
+                    await dataHandler.updateCard(cardId, boardId, statusId, newTitle, cardOrder);
+                } catch (error) {
+                    console.log(`There was an error during the board name update: ${error}`);
+                } finally {
+                    event.target.blur();
+                }
+            }
+        })
+    })
+}
+
 export let cardsManager = {
     loadCards: async function (boardId) {
         const cards = await dataHandler.getCardsByBoardId(boardId);
@@ -35,27 +57,7 @@ export let cardsManager = {
     cardRenameControl: function () {
         let cardTitleDivs = Array.from(document.querySelectorAll(".cardTitle"));
         cardTitleDivs.forEach(cardTitleDiv => {
-            cardTitleDiv.addEventListener("click", () => {
-                cardTitleDiv.addEventListener("keydown", async (event) => {
-                    if (event.key === "Enter") {
-                        event.preventDefault();
-                        let cardId = event.target.parentElement.dataset.cardId;
-                        let boardId = event.target.parentElement.dataset.boardId;
-                        let statusId = event.target.parentElement.dataset.statusId;
-                        let cardOrder = event.target.parentElement.dataset.cardOrder;
-                        let newTitle = cardTitleDiv.innerText;
-                        try {
-                            await dataHandler.updateCard(cardId, boardId, statusId, newTitle, cardOrder);
-                        }
-                        catch (error) {
-                            console.log(`There was an error during the board name update: ${error}`);
-                        }
-                        finally {
-                            event.target.blur();
-                        }
-                    }
-                })
-            })
+            initCardRename(cardTitleDiv);
         })
     },
 
@@ -72,34 +74,6 @@ export let cardsManager = {
 }
 };
 
-function renameCardHandler (cardDiv) {
-    let boardId = cardDiv.parentElement.parentElement.getAttribute("data-board-id");
-    let cardId = cardDiv.dataset["cardId"];
-    let cardTitle = cardDiv.innerText;
-    const formBuilder = htmlFactory(htmlTemplates.renameForm);
-    let newDiv = document.createElement("div");
-    newDiv.innerHTML = formBuilder(cardTitle);
-    newDiv.classList.add("card");
-
-    cardDiv.replaceWith(newDiv);
-
-    let inputField = document.querySelector(".card > form > input");
-    inputField.focus();
-
-    inputField.addEventListener("focusout", async () => {
-        cardDiv.innerText = inputField.value;
-        newDiv.replaceWith(cardDiv)
-        try {
-            let card = await dataHandler.getCard(cardId);
-            await dataHandler.updateCard(cardId, boardId, card.status_id, inputField.value, card.card_order);
-        }
-        catch (error) {
-            console.log(`There was an error during the card name update: ${error}`);
-        }
-    });
-
-}
-
 async function insertCard(boardId, statusId, cardOrder) {
     try{
         let response = await dataHandler.createNewCard(boardId, statusId, cardOrder);
@@ -111,9 +85,7 @@ async function insertCard(boardId, statusId, cardOrder) {
         domManager.addChild(`.board-column[data-column-id="${statusId}"]`, content);
         let cardElement = document.querySelector(`.board-column[data-column-id="${statusId}"]`).lastChild;
         initDraggable(cardElement);
-        cardElement.addEventListener("click", () => {
-                renameCardHandler(cardElement);
-            })
+        initCardRename(card.querySelector(".card-title"));
     }
     catch (error) {
         console.log("An error has occurred during card insertion:");
